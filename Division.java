dbcadc902122ab89da0efb6392a211fb502cc839
creestian <creestian@users.noreply.github.com>
2019-01-31 23:29:37 -0300

    Add files via upload

---
+++ Division.java
@@ -0,0 +1,12 @@
+package Calculate;
+
+public class Division implements doMath{
+	public void execute(Integer input1, Integer input2){
+		Integer division = input1 / input2;
+		System.out.println(division);
+	}
+	
+	public int division(int i, int j) {
+		return i / j;
+	}
+}
